c2c45b012c446d1c7c0fabde8dfd6054256efbb3
Júlio César <juliosbto@gmail.com>
2025-03-22 16:11:02 -0300

    Estilização e Ajustes
    Estilização geral do projeto e ajustes/correções.

---
+++ script/carrinho.js
@@ -0,0 +1,237 @@
+function adicionarItem(nome, preco, quantidade = 1) {
+    const itemExistente = Array.from(document.querySelectorAll(".item-row")).find((item) => {
+            return item.querySelector(".nome-produto").innerText === nome;
+        });
+    if (itemExistente) {
+        const inputQuantidade = itemExistente.querySelector(".quantidade-input");
+        inputQuantidade.value = parseInt(inputQuantidade.value) + quantidade;
+        atualizarTotal();
+        return;
+    }
+    const itemCarrinho = document.createElement("div");
+    itemCarrinho.classList.add("item-row");
+
+    itemCarrinho.innerHTML = `
+    
+    <div class="produto">
+        <img src="./assets/produtos/${nome}.jpg" alt="${nome}" class="imagem-produto" />
+        <span class="nome-produto">${nome} </span>
+    </div>
+
+    <div class="desconto">
+        <span class="valor-desconto">R$ 0,00</span>
+    </div>
+    
+    <div class="preco">
+        <span class="valor-preco">R$ ${preco.toFixed(2)}</span>
+    </div>
+
+    <div class="quantidade">
+           <input type="number" value="${quantidade}" min="1" class="quantidade-input" oninput="atualizarTotal()">
+        </div>
+
+        <div class="delete">
+            <button onclick="deletarItem(this)">Excluir</button>
+        </div>
+
+    
+    `;
+
+    /* adicionar item-carrinho */
+
+    document.getElementById("item-carrinho").appendChild(itemCarrinho);
+    atualizarTotal();
+}
+
+/* CARREGAR CARRINHO */
+function carregarCarrinho() {
+    const carrinho = JSON.parse(localStorage.getItem("carrinho")) || [];
+
+    carrinho.forEach((produto) => {
+        adicionarItem(produto.nome, produto.preco, produto.quantidade);
+    });
+}
+
+/* SALVAR CARRINHO */
+function salvarCarrinho() {
+    const carrinho = [];
+    const items = document.querySelectorAll(".item-row");
+
+    items.forEach((item) => {
+        const nome = item.querySelector(".nome-produto").innerText;
+        const preco = parseFloat(
+            item.querySelector(".valor-preco").innerText.replace("R$", "").trim()
+        );
+        const quantidade = parseInt(item.querySelector(".quantidade-input").value);
+        carrinho.push({ nome, preco, quantidade });
+    });
+
+    localStorage.setItem("carrinho", JSON.stringify(carrinho));
+}
+
+/* Validar FRETE */
+
+function consultarFrete() {
+    const cepInput = document.getElementById("cep-consulta").value;
+    const mensagemFrete = document.getElementById("mensagem-frete");
+
+    // Expressão regular para verificar se o CEP é numérico e tem 8 dígitos
+    const cepValido = /^[0-9]{5}-?[0-9]{3}$/;
+
+    // Listar CEPs inválidos
+    const cepsInvalidos = [
+        "00000-000",
+        "11111-111",
+        "22222-222",
+        "33333-333",
+        "44444-444",
+    ];
+
+    // Verificar se o CEP inserido é inválido
+    if (cepsInvalidos.includes(cepInput)) {
+        mensagemFrete.innerText = "Erro: Informe um CEP válido!";
+        mensagemFrete.style.color = "red";
+        return false;
+    } else if (cepValido.test(cepInput)) {
+        mensagemFrete.innerText =
+            "Seu CEP foi validado com sucesso! Frete grátis aplicado!";
+        mensagemFrete.style.color = "green";
+        return true;
+    } else {
+        mensagemFrete.innerText = "Erro: Informe um CEP válido!";
+        mensagemFrete.style.color = "red";
+        return false;
+    }
+}
+
+/* CUPOM DESCONTO */
+
+let cupomValido = "DESCONTO10";
+let desconto = 0;
+let totalCarrinho = 0;
+
+/* APLICAR CUPOM */
+function aplicarCupom() {
+    const cupomInserido = document.querySelector(".input-cupom").value.trim().toUpperCase();
+    const avisoCupom = document.getElementById("aviso-cupom");
+
+    const cuponsValidos = ["DESCONTO10"];
+
+    if (cuponsValidos.includes(cupomInserido)) {
+        desconto = 0.1;
+        avisoCupom.innerText = "Cupom aplicado com sucesso! Desconto de 10% ativado.";
+        avisoCupom.style.color = "green";
+        avisoCupom.style.display = "inline";
+    } else {
+        desconto = 0;
+        avisoCupom.innerText = "Cupom inválido!"
+        avisoCupom.style.color = "red"
+        avisoCupom.style.display = "inline";
+    }
+
+    atualizarTotal();
+}
+/* ATUALIZAR CARRINHO  */
+function atualizarTotal() {
+    let subtotal = 0;
+    const itens = document.querySelectorAll(".item-row");
+
+    itens.forEach((item) => {
+        const preco = parseFloat(
+            item.querySelector(".valor-preco").innerText.replace("R$", "").trim()
+        );
+        const quantidade = parseInt(item.querySelector(".quantidade-input").value);
+
+        if (!isNaN(preco) && !isNaN(quantidade)) {
+            subtotal += preco * quantidade;
+        }
+    });
+
+    let valorDesconto = subtotal * desconto;
+    totalCarrinho = subtotal - valorDesconto;
+
+    itens.forEach((item) => {
+        const preco = parseFloat(
+            item.querySelector(".valor-preco").innerText.replace("R$", "").trim()
+        );
+        const quantidade = parseInt(item.querySelector(".quantidade-input").value);
+        const descontoItem = preco * quantidade * desconto;
+
+        item.querySelector(
+            ".valor-desconto"
+        ).innerText = `R$ ${descontoItem.toFixed(2)}`;
+    });
+
+    /* ATUALIZAR EM DIV */
+    const totalCompra = document.getElementById("total-compra");
+    if (totalCompra) {
+        totalCompra.innerHTML =
+            subtotal > 0
+                ? `
+            <p>Subtotal: R$ ${subtotal.toFixed(2)}</p>
+            <p>Desconto: R$ ${valorDesconto.toFixed(2)}</p>
+            <p><strong>Total: R$ ${totalCarrinho.toFixed(2)}</strong></p>
+            
+        `
+                : `<p><strong> Carrinho Vazio</strong></p>`;
+
+        /* atualizar em localStorage o carrinho */
+        localStorage.setItem("totalCarrinho", totalCarrinho.toFixed(2));
+    }
+
+    salvarCarrinho();
+}
+
+/* DELETAR ITEMS */
+function deletarItem(button) {
+    let itemRow = button.closest(".item-row");
+    let nomeProduto = itemRow
+        ?.querySelector(".nome-produto")
+        ?.textContent?.trim();
+    if (!nomeProduto) return;
+
+    let itensSalvos = JSON.parse(localStorage.getItem("carrinho")) || [];
+    let novosItens = itensSalvos.filter((item) => item.nome !== nomeProduto);
+    localStorage.setItem("carrinho", JSON.stringify(novosItens));
+
+    itemRow.remove();
+    atualizarTotal();
+}
+
+/* FINALIZAR COMPRA */
+function finalizarCompra() {
+    const mensagemFrete = document.getElementById("mensagem-frete");
+    const avisoCompraFinalizada = document.getElementById(
+        "aviso-compra-finalizada"
+    );
+    const totalCompra = document.getElementById("total-compra");
+
+    // Verificar se o carrinho está vazio
+    let carrinho = JSON.parse(localStorage.getItem("carrinho")) || [];
+    if (carrinho.length === 0) {
+        avisoCompraFinalizada.innerText = "Erro: o carrinho está vazio!";
+        avisoCompraFinalizada.style.color = "red";
+        return;
+    }
+
+    const cepValido = consultarFrete();
+    if (!cepValido) {
+        avisoCompraFinalizada.innerHTML = "Erro: informe um cep válido!";
+        avisoCompraFinalizada.style.color = "red";
+        return;
+    }
+
+    avisoCompraFinalizada.innerHTML = "Compra Finalizada";
+    avisoCompraFinalizada.style.color = "green";
+}
+
+document
+    .querySelector(".btn-finalizar")
+    .addEventListener("click", finalizarCompra);
+
+/* LOAD ITEMS */
+
+window.addEventListener("load", () => {
+    carregarCarrinho();
+    atualizarTotal();
+});
---
+++ script/main.js
@@ -0,0 +1,84 @@
+const cartIcon = document.getElementById("cart-icon");
+
+    cartIcon.addEventListener("mouseenter", () => {
+    cartIcon.src = "./assets/cart_hover.svg";
+    });
+
+    cartIcon.addEventListener("mouseleave", () => {
+    cartIcon.src = "./assets/cart.svg";
+    });
+
+//produtos inicio
+  
+let produtosEmAlta = document.querySelector("#produtosEmAlta");
+let produtosNormal = document.querySelector("#produtosNormais");
+
+let ProdutosAlta = [
+    { nome: "Desinfetante e bactericida", descricao: "lorem ipsum", preco: 70 },
+    { nome: "Alimento natural para coelho", descricao: "lorem ipsum", preco: 36 },
+    { nome: "Cama preta Zee", descricao: "lorem ipsum", preco: 540 },
+    { nome: "Bebedouro", descricao: "lorem ipsum", preco: 75.2 },
+    { nome: "Mordedor galinha para cachorro", descricao: "lorem ipsum", preco: 8.7 },
+    { nome: "Mordedor bolinha", descricao: "lorem ipsum", preco: 7.6 }
+];
+
+let Produtos = [
+    { nome: "Cortador de unhas", descricao: "lorem ipsum", preco: 25.2 },
+    { nome: "Mini bifinho para cachorro", descricao: "lorem ipsum", preco: 25.5 },
+    { nome: "Tapete limpa patas", descricao: "lorem ipsum", preco: 56 },
+    { nome: "Coleira antipulgas", descricao: "lorem ipsum", preco: 200 },
+    { nome: "Bandeja preta para gatos", descricao: "lorem ipsum", preco: 20 },
+    { nome: "Fonte aqua mini", descricao: "lorem ipsum", preco: 96 }
+];
+        
+function RenderizarProdutos(lista, tipoLista){
+                
+    for(let produto of lista){
+        let cardProduto = document.createElement("div");
+        cardProduto.classList.add("card");
+        if(document.title == "Borcelle Pet-Shop"){
+        cardProduto.innerHTML =`
+           <img src="./assets/produtos/${produto.nome}.jpg" class="img-carrinho">
+            <p class="nome-carrinho" style="vertica">${produto.nome}</p>
+            <p class="preco-carrinho">Preço: R$ ${produto.preco.toFixed(2)}</p>
+            <button class="btn-add-carrinho" type="submit">Adicionar ao carrinho</button>
+        `;
+        tipoLista.appendChild(cardProduto);
+        
+        }else if(document.title == "Produtos"){
+            cardProduto.innerHTML +=`
+           <img src="./assets/produtos/${produto.nome}.jpg" class="img-carrinho">
+            <p class="nome-carrinho" style="vertica">${produto.nome}</p>
+            <p class="preco-carrinho">Preço: R$ ${produto.preco.toFixed(2)}</p>
+            <button class="btn-add-carrinho" type="submit">Adicionar ao carrinho</button>
+        `;
+        produtosTodos.appendChild(cardProduto);
+        }
+        
+        }
+};
+RenderizarProdutos(ProdutosAlta, produtosEmAlta);
+RenderizarProdutos(Produtos, produtosNormal);
+
+let produtosbtn = document.querySelectorAll(".btn-add-carrinho");
+produtosbtn.forEach(produto => {
+    produto.addEventListener('click', function(event) {
+        event.preventDefault();
+    
+    //pegando os dados dentro da div
+    let div = this.closest('div');
+    let paragraphs = div.querySelectorAll('p');
+    let nome = paragraphs[0].textContent.trim(); 
+    let preco = paragraphs[1].textContent.trim();
+    preco = Number(preco.slice(preco.indexOf("$")+1, preco.length));
+    preco = Number(preco.toFixed(2));
+
+    //jogando no localStorage
+    let produtoSelecionado = {nome, preco};
+    let carrinho = JSON.parse(localStorage.getItem('carrinho')) || [];
+        carrinho.push(produtoSelecionado);
+        localStorage.setItem('carrinho', JSON.stringify(carrinho));
+        alert("Produto adicionado ao carrinho!");
+    });
+});
+//produtos fim
